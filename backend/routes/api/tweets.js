be8dc29ef98a0b5d9ab06229b3ced0a261dd31b6
Jason <jasonzhang501@gmail.com>
2023-11-26 01:41:56 -0500

    Tweets Reading

--- backend/routes/api/tweets.js
+++ backend/routes/api/tweets.js
@@ -2,10 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 /* GET tweets listing. */
-router.get('/', function(req, res, next) {
-  res.json({
-    message: "GET /api/tweets"
-  });
+router.post('/', requireUser, validateTweetInput, async (req, res, next) => {
+  try {
+    const newTweet = new Tweet({
+      text: req.body.text,
+      author: req.user._id
+    });
+
+    let tweet = await newTweet.save();
+    tweet = await tweet.populate('author', '_id username');
+    return res.json(tweet);
+  }
+  catch(err) {
+    next(err);
+  }
 });
 
 module.exports = router;
---
+++ backend/validations/tweets.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const Tweet = mongoose.model('Tweet');
+const { requireUser } = require('../../config/passport');
+const validateTweetInput = require('../../validations/tweets');
+
+router.get('/', async (req, res) => {
+  try {
+    const tweets = await Tweet.find()
+                              .populate("author", "_id username")
+                              .sort({ createdAt: -1 });
+    return res.json(tweets);
+  }
+  catch(err) {
+    return res.json([]);
+  }
+});
+
+router.get('/user/:userId', async (req, res, next) => {
+  let user;
+  try {
+    user = await User.findById(req.params.userId);
+  } catch(err) {
+    const error = new Error('User not found');
+    error.statusCode = 404;
+    error.errors = { message: "No user found with that id" };
+    return next(error);
+  }
+  try {
+    const tweets = await Tweet.find({ author: user._id })
+                              .sort({ createdAt: -1 })
+                              .populate("author", "_id username");
+    return res.json(tweets);
+  }
+  catch(err) {
+    return res.json([]);
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const tweet = await Tweet.findById(req.params.id)
+                             .populate("author", "_id username");
+    return res.json(tweet);
+  }
+  catch(err) {
+    const error = new Error('Tweet not found');
+    error.statusCode = 404;
+    error.errors = { message: "No tweet found with that id" };
+    return next(error);
+  }
+});
